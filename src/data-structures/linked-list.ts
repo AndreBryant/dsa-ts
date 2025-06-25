53a59734d0ba47aae1a68ff0d3df7f17ad4e12b0
embryzon <95128949+embryzon@users.noreply.github.com>
2025-06-25 10:27:04 +0800

    Add remaining singly linked list methods

--- src/data-structures/linked-list.ts
+++ src/data-structures/linked-list.ts
@@ -8,7 +8,7 @@ class MyNode {
   }
 }
 
-class SinglyLinkedList {
+export class SinglyLinkedList {
   private head: MyNode | null;
   private tail: MyNode | null;
 
@@ -17,6 +17,7 @@ class SinglyLinkedList {
     this.tail = this.head;
   }
 
+  //   void: O(n)
   public insert(value: number) {
     const n = new MyNode(value);
     if (this.head === null) {
@@ -26,5 +27,103 @@ class SinglyLinkedList {
       this.tail.next = n;
       this.tail = n;
     }
+  }
+
+  //   boolean: O(n)
+  public contains(value: number) {
+    let n = this.head;
+
+    while (n !== null && n.value !== value) {
+      n = n.next;
+    }
+
+    if (n === null) return false;
+
+    return true;
+  }
+
+  //   void: returns true if success otherwise, a fail
+  public remove(value: number) {
+    /*
+        Cases to consider
+        1 empty list
+        2 if linked list has only one node and is the one to be removed
+        3 removing the head node
+        4 removing the tail node
+        5 the node to be removed is somewhere in the middle of the head and tail
+        6 the node to be removed is not in the linked list
+    */
+
+    // Case 1
+    if (this.head === null) return false;
+
+    let n = this.head;
+
+    // Case 2 and 3
+    if (n.value === value) {
+      if (this.head == this.tail) {
+        // If list contains only one node
+        this.head = null;
+        this.tail = null;
+      } else {
+        // if node to be removed is in the head
+        this.head = this.head.next;
+      }
+      return true;
+    }
+
+    // iterate over the list to find a matching value
+    while (n.next !== null && n.next.value !== value) {
+      n = n.next;
+    }
+
+    if (n.next !== null) {
+      // remove the tail
+      if (n.next === this.tail) this.tail = n;
+      // case 5
+      n.next = n.next.next;
+      return true;
+    }
+    // case 6
+    return false;
+  }
+
+  //   void
+  public traverse() {
+    console.log('Start of Traversal');
+
+    let n = this.head;
+
+    process.stdout.write('Values: ');
+    while (n !== null) {
+      process.stdout.write(n.value.toString() + ' -> ');
+      n = n.next;
+    }
+
+    console.log('null\nEnd of traversal');
+  }
+
+  public reverseTraversal() {
+    console.log('Start of Reverse Traversal');
+
+    process.stdout.write('Values: ');
+    if (this.tail !== null) {
+      let curr = this.tail;
+
+      while (curr !== this.head) {
+        let prev = this.head;
+
+        while (prev.next !== curr) {
+          prev = prev.next;
+        }
+
+        process.stdout.write(curr.value.toString() + ' <- ');
+        curr = prev;
+      }
+
+      process.stdout.write(curr.value.toString());
+    }
+
+    console.log('\nEnd of reverse traversal');
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("test");
+import { SinglyLinkedList } from './data-structures/linked-list';
+
+const singlyLinkedList = new SinglyLinkedList(1);
+
+singlyLinkedList.insert(2);
+singlyLinkedList.insert(3);
+singlyLinkedList.insert(4);
+singlyLinkedList.traverse();
+
+singlyLinkedList.remove(3);
+singlyLinkedList.reverseTraversal();
